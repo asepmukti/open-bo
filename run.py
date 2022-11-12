23db6c5250c27c824b73a5edb73cc574e67f2498
Boy Hamzah <88397313+BOY-H4MZ4H@users.noreply.github.com>
2022-11-12 17:12:42 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("bo").infoo()
+       __import__("bo").clear()
    except Exception as e:
        exit(str(e))
